661457c455085c46cb3f9a0f87db18e4a48f6ba3
GiadaMarzapane <giada.ortesta@gmail.com>
2023-03-14 20:42:13 +0100

    Stampati tutti gli elementi in pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,24 @@
         <title>Laravel Migration Seeder</title>
     </head>
     <body>
-        <h1 class="text-center">Laravel Migration Seeder</h1>
+        <h1 class="text-center mb-5">Laravel Migration Seeder</h1>
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div>
+                        @foreach ($trains as $element)
+                            <div class="d-flex justify-content-evenly border-bottom">
+                                <p style="width: calc(100% / 5)">{{ $element->azienda}}</p>
+                                <p style="width: calc(100% / 5)">{{ $element->stazione_partenza}} -></p>
+                                <p style="width: calc(100% / 5)">{{ $element->stazione_arrivo}}</p>
+                                <p style="width: calc(100% / 5)">{{ $element->orario_partenza}}</p>
+                                <p style="width: calc(100% / 5)">{{ $element->orario_arrivo}}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
